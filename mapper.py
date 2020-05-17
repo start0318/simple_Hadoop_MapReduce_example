856f0648b143b2c87244fa6a7c90fd38ebc2f05b
start0318 <startboyce02052018@gmail.com>
2020-05-17 15:29:52 +0000

    lowercase, no punctuation, no stopwords

--- mapper.py
+++ mapper.py
@@ -1,14 +1,23 @@
 #!/usr/bin/env python
 import sys
+import re
+from sklearn.feature_extraction import stop_words
+#stopwords = set(['the','and'])
+stopwords = set(stop_words.ENGLISH_STOP_WORDS) 
 
 # get all lines from stdin
 for line in sys.stdin:
     # remove leading and trailing whitespace
-    line = line.strip()
+    line = line.strip().lower()
+
+    # remove punctuation
+    line  = re.sub(r'[^\w\s]','',line)
 
     # split the line into words; splits on any whitespace
     words = line.split()
 
     # output tuples (word, 1) in tab-delimited format
+    
     for word in words:
-        print '%s\t%s' % (word, "1")
+	if word not in stopwords:
+           print '%s\t%s' % (word, "1")
